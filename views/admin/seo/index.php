795f6822fcf60e11a75e5fade1b0ac71248e4de3
Chris Gilley <chris@gilleydigital.com>
2013-09-23 21:07:29 -0400

    Added formaid namespace div

--- views/admin/seo/index.php
+++ views/admin/seo/index.php
@@ -1,3 +1,5 @@
-<?php echo View::factory('formaid/messages') ?>
-<div class="spreadsheet"></div>
-<?php echo Form::submit(NULL, 'Save', array('class' => 'submit')) ?>
+<div class="formaid">
+	<?php echo View::factory('formaid/messages') ?>
+	<div class="spreadsheet"></div>
+	<?php echo Form::submit(NULL, 'Save', array('class' => 'formfield-submit')) ?>
+</div>
